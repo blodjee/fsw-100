c3b7ac989b3d70ffd1cb09fe61a4c6742745efbf
blodjee <63461569+blodjee@users.noreply.github.com>
2020-08-16 13:26:56 -0400

    Hello

---
+++ week1/grocery_storeJS/shoppingCart2/index.js
@@ -0,0 +1,14 @@
+/* 
+index.js 
+ */
+
+ var shopper =  {
+    man : 'fish',
+    woman: 'carrot',
+    price: 650,
+    isAvailable: true,
+    items: function() {
+        return this.man + ' ' + this.woman
+    }
+ }
+
