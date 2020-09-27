0ff5cd7b33ce5fb846c20130fd11f6db3845133a
blodjee <63461569+blodjee@users.noreply.github.com>
2020-09-26 22:26:25 -0400

    hello

---
+++ week1/grocery_storeJS/index.js
@@ -0,0 +1,24 @@
+
+       
+    
+        let shopper =  {
+            man : "lobster",
+            woman: "beef",
+            price: 150,
+            deduct: 25,
+            isAvailable: true,
+            gorceryCart: ["orange", "peach", "salchichas"],
+            finalPrice: function() {
+                return this.price - this.deduct
+            },
+            items: function() {
+                return this.man + ' ' + this.woman
+            }
+        }
+
+
+        document.getElementById("man").innerHTML = "The man bought a" + " " + shopper.man;
+        document.getElementById("woman").innerHTML = "The woman bought a " + shopper.woman;
+        document.getElementById("finalPrice").append(shopper.finalPrice());
+        document.getElementById('items').innerHTML  = "Here are the items i bought this week" + " " + shopper.gorceryCart;
+
