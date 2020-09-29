004a7c335d7f0dc16375084e0413fd0e62218c4c
blodjee <63461569+blodjee@users.noreply.github.com>
2020-09-28 21:12:47 -0400

    hello

---
+++ week2/creating&Appending_elements/index.js
@@ -0,0 +1,12 @@
+let newLi = document.createElement("li")
+newLi.textContent = "react.js"
+let helloList = document.getElementById("hello-list")
+helloList.append(newLi)
+
+
+let helloP = document.createElement("helloP")
+helloP.textContent = "All of this was created with Javascript"
+helloP.style.textAlign = "center"
+helloP.style.fontSize = "30px"
+document.body.append(helloP)
+
