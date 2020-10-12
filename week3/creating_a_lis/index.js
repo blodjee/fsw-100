df220395fad6c5203aa44414c1388d70bec7f305
blodjee <63461569+blodjee@users.noreply.github.com>
2020-10-12 02:25:47 -0400

    hello

---
+++ week3/creating_a_lis/index.js
@@ -0,0 +1,12 @@
+
+        var text = document.querySelector(".world")
+        text.style.color = "red";
+
+        
+        var arr = ["steve","larry","joe","shirlee","steph","nate","rick","emily"];
+        var hello ="";
+        for (var i = 0; i < arr.length; i++){
+            hello += arr[i]
+            
+        }
+        
